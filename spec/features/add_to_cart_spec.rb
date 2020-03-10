0edec5eafcbe3ccc6d11e7f1f30e11e724691449
QuintonEL <quintonlaborde@gmail.com>
2020-03-10 16:55:26 +0000

    implemented add to cart feature spec

--- spec/features/add_to_cart_spec.rb
+++ spec/features/add_to_cart_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature, js: true do
+RSpec.feature "AddToCarts", type: :feature, js: true do
   # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
@@ -14,15 +14,15 @@ RSpec.feature "ProductDetails", type: :feature, js: true do
       )
   end
 
-  scenario "They see product details" do
+  scenario "They see an item in their cart" do
 
     visit root_path
 
-    find('a.btn.btn-default').click
+    find('button.btn.btn-primary').click
     # commented out b/c it's for debugging only
     
-    page.has_content?('Description')
-    puts page.html
+    page.has_content?('My Cart (1)')
     save_and_open_screenshot
+    puts page.html
   end
 end
